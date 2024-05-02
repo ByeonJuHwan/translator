1600a612560028a174f85baf899f7db3b4c6995d
juhwan <asdcz@naver.com>
2024-05-02 22:23:31 +0900

    메인 페이지 컨트롤러, view, test 추가

---
+++ src/test/java/com/byeon/translator/controller/page/MainControllerTest.java
@@ -0,0 +1,31 @@
+package com.byeon.translator.controller.page;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@DisplayName("홈페이지 컨트롤러 테스트")
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    @DisplayName("[View] 메인 페이지 이동")
+    void main() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("main"));
+    }
+}
