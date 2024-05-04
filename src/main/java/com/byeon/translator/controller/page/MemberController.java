ffd095981a51cfc107ac4fef9c21c53753605f87
juhwan <asdcz@naver.com>
2024-05-04 12:07:57 +0900

    회원가입 컨트롤러 테스트 작성 및 컨트롤러

--- src/main/java/com/byeon/translator/controller/page/MemberController.java
+++ src/main/java/com/byeon/translator/controller/page/MemberController.java
@@ -3,17 +3,26 @@ package com.byeon.translator.controller.page;
 
 import com.byeon.translator.controller.request.MemberJoinRequest;
 import com.byeon.translator.controller.response.MemberJoinResponse;
+import com.byeon.translator.service.member.front.MemberFrontService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberFrontService memberFrontService;
 
     @GetMapping("/join")
     public String joinPage(Model model) {
@@ -22,8 +31,15 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String join(MemberJoinRequest request, Model model) {
-        return "main";
-    }
+    public String join(@Validated @ModelAttribute("member") MemberJoinRequest request, BindingResult bindingResult) {
 
+        if (bindingResult.hasErrors()) {
+            log.error("error = {}", bindingResult.getAllErrors());
+            return "member/join";
+        }
+
+        memberFrontService.join(request);
+
+        return "redirect:/";
+    }
 }
--- src/test/java/com/byeon/translator/controller/page/MemberControllerTest.java
+++ src/test/java/com/byeon/translator/controller/page/MemberControllerTest.java
@@ -1,37 +1,86 @@
 package com.byeon.translator.controller.page;
 
+import com.byeon.translator.AbstractContainerBaseTest;
 import com.byeon.translator.controller.request.MemberJoinRequest;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.byeon.translator.service.member.front.MemberFrontService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View - 회원 컨트롤러")
-@WebMvcTest(MemberController.class)
-class MemberControllerTest {
+@AutoConfigureMockMvc
+class MemberControllerTest extends AbstractContainerBaseTest{
 
-    private final MockMvc mvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-    MemberControllerTest(@Autowired MockMvc mvc) {
-        this.mvc = mvc;
-    }
+    @MockBean
+    private MemberFrontService memberFrontService;
+
 
     @Test
-    @DisplayName("회원가입 페이지 이동")
+    @WithAnonymousUser
+    @DisplayName("[회원가입] 회원가입 페이지 이동")
     void moveJoinPageTest() throws Exception {
-        mvc.perform(get("/join"))
+        mockMvc.perform(get("/member/join"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("member/join"))
                 .andExpect(model().attributeExists("member"))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
+    @Test
+    @WithAnonymousUser
+    @DisplayName("[회원가입] 회원 가입 성공 후 메인 페이지로 이동")
+    void doJoinTest() throws Exception {
+        MemberJoinRequest request = validRequest();
+        doNothing().when(memberFrontService).join(any(MemberJoinRequest.class));
+
+        mockMvc.perform(post("/member/join")
+                        .param("userId", request.getUserId())
+                        .param("password", request.getPassword())
+                        .param("name", request.getName()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    @WithAnonymousUser
+    @DisplayName("[회원가입] 회원 가입 시 아이디 비밀번호 validation 테스트")
+    void requestValidationCheckTest() throws Exception {
+        MemberJoinRequest request = invalidRequest();
+
+        doNothing().when(memberFrontService).join(any(MemberJoinRequest.class));
+
+        mockMvc.perform(post("/member/join")
+                        .param("userId", request.getUserId())
+                        .param("password", request.getPassword())
+                        .param("name", request.getName()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("member/join"))
+                .andExpect(model().attributeExists("member"))
+                .andExpect(model().attributeHasFieldErrors("member", "userId"))
+                .andExpect(model().attributeHasFieldErrors("member", "password"))
+                .andExpect(model().attributeHasFieldErrors("member", "name"));
+
+    }
+
+    private MemberJoinRequest validRequest() {
+        return new MemberJoinRequest("test", "test" ,"test");
+    }
+
+    private MemberJoinRequest invalidRequest() {
+        return new MemberJoinRequest("", "" ,"");
+    }
 }
