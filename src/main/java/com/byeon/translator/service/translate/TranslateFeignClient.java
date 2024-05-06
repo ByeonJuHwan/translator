85b085e25aa8ae6d5cc5c3dd80b694fa82241f38
juhwan <asdcz@naver.com>
2024-05-06 15:42:42 +0900

    단어장 기능 추가 하면서 발경된 오타수정

--- src/main/java/com/byeon/translator/service/translate/TranslateFeignClient.java
+++ src/main/java/com/byeon/translator/service/translate/TranslateFeignClient.java
@@ -1,19 +1,11 @@
 package com.byeon.translator.service.translate;
 
-import com.byeon.translator.controller.request.translate.TranslateRequest;
-import com.byeon.translator.controller.response.DeeplResponse;
+import com.byeon.translator.controller.response.deepl.DeeplResponse;
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.SpringQueryMap;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Map;
 
 @FeignClient(
         name = "translate-client",
