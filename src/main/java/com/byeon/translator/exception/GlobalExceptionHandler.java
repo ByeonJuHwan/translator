19290e5a5b374b6aa597503536efeeb7b3e1880c
juhwan <asdcz@naver.com>
2024-05-06 16:49:02 +0900

    멤버가 없는 경우의 예외 추가

--- src/main/java/com/byeon/translator/exception/GlobalExceptionHandler.java
+++ src/main/java/com/byeon/translator/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.byeon.translator.exception;
 
 import com.byeon.translator.controller.response.MemberJoinResponse;
 import com.byeon.translator.exception.custom.DuplicatedMemberException;
+import com.byeon.translator.exception.custom.MemberNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +14,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DuplicatedMemberException.class)
     public String handleDuplicatedMemberException(DuplicatedMemberException e, Model model) {
+        model.addAttribute("userIdError", e.getMessage());
+        model.addAttribute("member", new MemberJoinResponse());
+        return "member/join";
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public String handleMemberNotFoundException(MemberNotFoundException e, Model model) {
         model.addAttribute("userIdError", e.getMessage());
         model.addAttribute("member", new MemberJoinResponse());
         return "member/join";
